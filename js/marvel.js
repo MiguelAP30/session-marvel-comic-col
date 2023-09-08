8428efc7e8403a3ad027efd3e8024e52d79f2dd8
AbelSantiagoG <abelsantiago1000@gmail.com>
2023-09-07 22:27:11 -0500

    table edited in css and shaped

--- js/marvel.js
+++ js/marvel.js
@@ -1,27 +1,24 @@
-const url = "https://gateway.marvel.com/v1/public/series?ts=1&apikey=23f6f9ee394ee575f1f160012f5064b8&hash=3ee57ae9fd9f8053a3a6909d5a64f5b5"
+/* const url = "https://gateway.marvel.com/v1/public/series?ts=1&apikey=23f6f9ee394ee575f1f160012f5064b8&hash=3ee57ae9fd9f8053a3a6909d5a64f5b5"
 fetch(url)
     .then((response)=>response.json())
     .then((valor)=>{
         const apiInformation= valor.data.results;
-        const MarvelContainer = document.querySelector(".nombre");
+        const MarvelContainer = document.querySelector(".tabla");
         MarvelContainer.innerHTML="";
-        const tableMarvel= document.createElement("table")
         tableMarvel.classList.add("table-information")
         apiInformation.forEach((resultado)=>{
-            const MarvelTitulo = document.createElement("p");
+            const MarvelTitulo = document.createElement("tr");
             MarvelTitulo.textContent= "cartelera.title";
-            const CarteleraContainer = document.createElement("div");
-            CarteleraContainer.classList.add("cartelera-marvel");
+            const casillaNombre1 = document.createElement("td");
+            casillaNombre1.classList.add("cartelera-marvel");
             const MarvelUrl = document.createElement("p");
             MarvelUrl.textContent= "cartelera.urls[0].url";
 
-            CarteleraContainer.appendChild(MarvelTitulo);
-            CarteleraContainer.appendChild(MarvelUrl);
-            MarvelContainer.appendChild(CarteleraContainer);
+            casillaNombre1.appendChild(MarvelTitulo);
+            casillaNombre1.appendChild(MarvelUrl);
+            MarvelContainer.appendChild(casillaNombre1);
         })
-        MarvelContainer.appendChild(tableMarvel)
         console.log("202");
     })
-    .catch((error)=> console.log("Error 404"));
-
+    .catch((error)=> console.log("Error 404")); */
 
